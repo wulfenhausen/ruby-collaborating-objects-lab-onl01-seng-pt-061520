28dbc4f21b0c8a82f8bcc5b7b5ff2079fb14c017
William Han Findiesen <william.findiesen@gmail.com>
2020-07-18 20:19:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,37 @@
 class Artist
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  # other methods
- 
-end
+    attr_accessor :name, :songs
+    @@all = []
+    def initialize(name)
+        @name = name 
+        @songs = []
+        save
+    end 
+    def self.all 
+        @@all 
+    end 
+    def self.create(name)
+        artist = self.new(name)
+        artist.name = name 
+        artist
+    end 
+    def self.find(name)
+        self.all.detect { |artist| artist.name == name}
+    end 
+    def self.find_or_create_by_name(name)
+        self.find(name) || self.create(name)
+    end 
+    def add_song(song)
+        @songs << song
+    end 
+    def print_songs
+        @songs.each do |song|
+            puts song.name
+        end 
+    end 
+    def save 
+        @@all << self
+    end
+    def songs
+        @songs
+    end 
+end 
