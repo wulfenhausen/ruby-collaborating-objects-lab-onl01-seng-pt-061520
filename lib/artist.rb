cdd303cdb318b88038c60379c483a0fbc38afd56
William Han Findiesen <william.findiesen@gmail.com>
2020-07-18 20:28:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,17 +14,15 @@ class Artist
         artist.name = name 
         artist
     end 
-   # def self.find(name)
-     #   self.all.detect { |artist| artist.name == name}
-   # end 
+    def self.find(name)
+        self.all.detect { |artist| artist.name == name}
+   end 
+    
+  
     
     def self.find_or_create_by_name(name)
-      self.select(name) || self.create(name)
-    end
-    
-  #  def self.find_or_create_by_name(name)
-   #     self.find(name) || self.create(name)
-  #  end 
+       self.find(name) || self.create(name)
+    end 
     def add_song(song)
         @songs << song
     end 
