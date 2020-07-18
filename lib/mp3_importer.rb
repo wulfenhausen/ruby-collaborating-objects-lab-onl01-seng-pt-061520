e27edd0a3598f706cd73dd23e068346e6f5a1522
William Han Findiesen <william.findiesen@gmail.com>
2020-07-18 20:34:44 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,12 @@
 class MP3Importer
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-  end
+    attr_accessor :path
+    def initialize(path)
+        @path = path 
+    end 
+    def files 
+        Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+    end 
+    def import
+        self.files.each {|file| Song.new_by_filename(file)}
+    end 
 end
